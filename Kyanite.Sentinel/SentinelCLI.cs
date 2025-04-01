2118ed973cabc4174d3e6fdd4892c583e9c17442
Jewels <jewels286@proton.me>
2025-04-01 19:00:26 -0400

    initial commits

---
+++ Kyanite.Sentinel/SentinelCLI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spectre.Console;
+using Kyanite.Sentinel.Utils;
+
+namespace Kyanite.Sentinel
+{
+	internal static class SentinelCLI
+	{
+		public const int LINES = 0;
+		public const int VECTOR_CONNECTION_STEPS = 0;
+
+		public static async Task Run()
+		{
+			AnsiConsole.MarkupLine("Welcome to [blue]Kyanite Sentinel[/]!");
+			AnsiConsole.MarkupLine($"To connect from [green]Kyanite Vector[/], use the address [bold]{GeneralUtilities.GetLocalIPAddress()}[/]:{GeneralUtilities.GenerateRandomPort()}");
+			
+
+		}
+	}
+}
---
+++ Kyanite.Sentinel/Utils/GeneralUtilities.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.
+
+
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace Kyanite.Sentinel.Utils
+{
+	internal static class GeneralUtilities
+	{
+		internal static string GetLocalIPAddress()
+		{
+			foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+			{
+				if (ni.OperationalStatus == OperationalStatus.Up &&
+					(ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ||
+					 ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet))
+				{
+					foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+					{
+						if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+						{
+							return ip.Address.ToString();
+						}
+					}
+				}
+			}
+
+			throw new Exception("No active network adapters with an IPv4 address found.");
+		}
+
+		internal static int GenerateRandomPort()
+		{
+			Random random = new Random();
+			return random.Next(49152, 65535);
+		}
+	}
+}
